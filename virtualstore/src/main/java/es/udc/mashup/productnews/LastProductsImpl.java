1f76c401f11342b0537eba5e94e5ed22d6cf6499
hyandarie <hyandarie@gmail.com>
2015-04-16 20:53:27 +0200

    Subido LasProducts

--- virtualstore/src/main/java/es/udc/mashup/productnews/LastProductsImpl.java
+++ virtualstore/src/main/java/es/udc/mashup/productnews/LastProductsImpl.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
 
@@ -22,9 +23,23 @@ public class LastProductsImpl implements LastProductsService {
 		
 		Date timelimit = cal.getTime();
 
-		Element products = new Element("products");
+		Element products = new Element("feed");
+		Attribute attr1 = new Attribute("xmlns", "http://www.w3.org/2005/Atom");
+        products.setAttribute(attr1);
 		Document doc = new Document(products);
 		doc.setRootElement(products);
+		Element title = new Element("title").setText("Last products");
+		doc.getRootElement().addContent(title);
+		Element link = new Element("link");
+		Attribute attr2 = new Attribute("href", "http://www.acme.com/lastproducts.atom");
+        products.setAttribute(attr2);
+		doc.getRootElement().addContent(link);
+		Element subtitle = new Element("subtitle").setText("Products added in the last 24 hours");
+		doc.getRootElement().addContent(subtitle);
+		Element updated = new Element("updated").setText(Calendar.getInstance().getTime().toString());
+		doc.getRootElement().addContent(updated);
+		Element id = new Element("id").setText("PracticaADOO");
+		//doc.getRootElement().addContent(id);
 
 		Iterator<ProductTO> nombreIterator = lista.iterator();
 		while (nombreIterator.hasNext()) {
@@ -32,9 +47,12 @@ public class LastProductsImpl implements LastProductsService {
 
 			if (elemento.getDate().after(timelimit)) 
 			{
-				Element product = new Element("product");
+				Element product = new Element("entry");
 				product.addContent(new Element("title").setText(elemento.getName()));
-				product.addContent(new Element("summary").setText(elemento.getDescription()));
+				//product.addContent(new Element("id").setText(elemento.getName()));
+				//product.addContent(new Element("link").setAttribute("href", ""));
+				String summary = elemento.getDescription() + " - " + elemento.getPrice() + " - " + elemento.getCategory();
+				product.addContent(new Element("summary").setText(summary));
 				product.addContent(new Element("updated").setText(elemento.getDate().toString()));
 
 				doc.getRootElement().addContent(product);
